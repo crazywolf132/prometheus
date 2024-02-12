39bf9f9ef36f30dbd2bc390818b1a992a6cc3325
crazywolf132 <crazywolf132@gmail.com>
2024-02-12 16:12:49 +1100

    chore: made `runNanoapp` safer

--- runNanoapp.sh
+++ runNanoapp.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # Ask for the app name
-read -p "Enter the app name: " APP_NAME
+while true; do
+    read -p "Enter the app name: " APP_NAME
+
+    # Check if the app name is 'landing'
+    if [ "$APP_NAME" = "landing" ]; then
+        echo "Please run the ./runShell.sh script instead."
+        exit 1
+    fi
+
+    # Check if the app name is one of the folders in the ./apps directory
+    if [ -d "./apps/$APP_NAME" ]; then
+        break
+    else
+        echo "Invalid app name. Please try again."
+    fi
+done
 
 # Change directory to the app location
 cd "./apps/$APP_NAME"
