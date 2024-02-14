6cfd70bd74c9f3fd024e5d053d129ad9dff056cd
crazywolf132 <crazywolf132@gmail.com>
2024-02-14 11:24:30 +1100

    fix: fixed broken `LoadApp`

--- packages/runtime/src/components/LoadApp.tsx
+++ packages/runtime/src/components/LoadApp.tsx
@@ -1,6 +1,6 @@
-import type { Bundle, NanoAppFunction } from "@types";
-import { execute, returnAppAsElement, fetchWithCache } from "@utils";
+import { execute, fetchWithCache, returnAppAsElement } from "@utils";
 // @ts-ignore; No type files for this dep
+import { NanoAppFunction } from "@types";
 import cloneOrCreate from "clone-or-create";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -19,7 +19,7 @@ export const LoadApp = React.memo((props: Props) => {
   const [styles, setStyles] = React.useState<string>("");
   const Spinner = props.spinner ?? Placeholder;
 
-  const loadApp = async (shadowRoot: ShadowRoot) => {
+  const loadApp = React.useCallback(async (shadowRoot: ShadowRoot) => {
     try {
       const bundleUrl = `http://localhost:5000/bundles/${props.appName}`;
       const bundle = await fetchWithCache(bundleUrl);
@@ -33,7 +33,7 @@ export const LoadApp = React.memo((props: Props) => {
           ...props.internal,
           errorBoundary: props.errorBoundary ?? Placeholder,
         },
-      });
+      }) as NanoAppFunction;
 
       returnAppAsElement(nanoApp, `root_container_${props.appName}`, `${props.appName}_parent_container`, shadowRoot);
       setStyles(nanoApp.styles);
@@ -41,19 +41,18 @@ export const LoadApp = React.memo((props: Props) => {
     } catch (error) {
       console.error("Error loading app:", error);
     }
-  };
+  }, [props.appName, props.errorBoundary, props.internal]);
 
   const ErrorLayer = (props.errorBoundary || Placeholder) as any; // Because ts is pretty shit
 
   return (
     <div id={`${props.appName}_root`} style={{ display: 'flex', height: '100%', width: '100%' }}>
       <ErrorLayer>
-        <ShadowElement onShadowRootReady={load}>
+        <ShadowElement onShadowRootReady={loadApp}>
           <section id={`${props.appName}_parent_container`} style={{ display: 'flex', flex: 1, height: '100%', width: '100%' }}>
             <style>{styles}</style>
             <div id={`root_container_${props.appName}`} style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
               <Spinner id="root_container_spinner"/>
-               {/* {cloneOrCreate(props.spinner, { id: "root_container_spinner" })} */}
             </div>
           </section>
         </ShadowElement>
